361335b4025abf42578c2ed57d344c6ee911b81f
정유빈 <yubeen.jeong@aiaracorp.com>
2019-06-16 00:49:27 +0900

    캐릭터 컨트롤러 / 캐릭터 이동 구현

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    #region PROPERTIES
+    private readonly float gravity = -9.8f;
+    public bool hasController = true;
+    public Camera playerCamera;
+    public VariableJoystick MoveInput;
+    public VariableJoystick ActionInput;
+    public Transform chracterRoot;
+    [HideInInspector]
+    public CharacterController characterController;
+    [HideInInspector]
+    public Animator animator;
+
+    public float speed = 5;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitalComponent();
+    }
+
+    #region INITAL
+    private void InitalComponent() {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        if (!hasController)
+        {
+            Destroy(playerCamera.gameObject);
+        }
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveControl();
+        UpdateAnimation();
+    }
+
+    #region CHARACTOR_UPDATE
+    private void MoveControl() {
+        Debug.Log("Move:");
+        Debug.Log(MoveInput.Direction);
+        Vector3 input = Vector3.zero;
+        input += transform.forward * MoveInput.Vertical;
+        input += transform.right * MoveInput.Horizontal;
+        input = ApplyGravity(input);
+        characterController.Move(input * speed * Time.deltaTime);
+        var characterDegree = Mathf.Atan2(MoveInput.Horizontal,MoveInput.Vertical) * Mathf.Rad2Deg;
+        chracterRoot.rotation = Quaternion.Euler(new Vector3(0,characterDegree,0));
+    }
+
+    private Vector3 ApplyGravity(Vector3 input) {
+        var source = input;
+        if (!characterController.isGrounded)
+        {
+            source += new Vector3(0, gravity, 0);
+        }
+        return source;
+    }
+
+    private void UpdateAnimation() 
+    {
+        animator.SetFloat("Horizontal", Math.Abs(MoveInput.Horizontal));
+        animator.SetFloat("Vertical", Math.Abs(MoveInput.Vertical));
+    }
+    #endregion
+}
