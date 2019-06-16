142566292269bf4e07c4956aa9b1c721c182882e
정유빈 <yubeen.jeong@aiaracorp.com>
2019-06-16 21:58:39 +0900

    인게임 네트워크 플로우 컨트롤러 추가

---
+++ Assets/Scripts/MainSceneController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MainSceneController : MonoBehaviourPunCallbacks {
+    public Transform[] spawnSpot;
+    public static GameObject player;
+    public readonly string version = "1.0.0";
+
+    #region MonoBehavior LifeCycle
+    public void Awake() {
+        PhotonNetwork.AddCallbackTarget(this);
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void Start() {
+        PhotonConnect();
+    }
+
+    private void OnDestroy() {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+    #endregion
+
+    private void SpawnPlayer() {
+        var spawnIndex = UnityEngine.Random.Range(0, spawnSpot.Length);
+        player = PhotonNetwork.Instantiate("Player", spawnSpot[spawnIndex].position, Quaternion.identity);
+    }
+
+    #region Photon LifeCycle
+    private void PhotonConnect() {
+        PhotonNetwork.GameVersion = version;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom() {
+        if (player == null)
+        {
+            SpawnPlayer();
+        }
+    }
+    #endregion
+}
