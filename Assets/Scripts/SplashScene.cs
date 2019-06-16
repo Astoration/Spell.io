1fd27aba7d57665b5ed3d59d88f98e67b249b41f
정유빈 <yubeen.jeong@aiaracorp.com>
2019-06-16 20:59:52 +0900

    스플래쉬 화면에 고양이 소리 추가

--- Assets/Scripts/SplashScene.cs
+++ Assets/Scripts/SplashScene.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SplashScene : MonoBehaviour
 {
+    public AudioSource sfxAudioSource;
+
+    public void PlaySFX()
+    {
+        sfxAudioSource.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
